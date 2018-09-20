6e7a0277d4ff9f0ed1b4f093b8acf52faba3c0c6
frederik vannoote <frederik.vannoote@barco.com>
2018-09-20 21:42:01 +0200

    Add functionality to create an archive containing the tests.

--- AddQtTest.cmake
+++ AddQtTest.cmake
@@ -1,3 +1,29 @@
+# Remove any previous file. Start with a clean slate.
+file(REMOVE ${CMAKE_BINARY_DIR}/tests.txt)
+file(REMOVE ${CMAKE_BINARY_DIR}/private_tests.txt)
+file(REMOVE ${CMAKE_BINARY_DIR}/manual_tests.txt)
+
+if(NOT TARGET manual-test-archive)
+    add_custom_target(manual-test-archive
+        COMMAND ${CMAKE_COMMAND} -E remove -f "${CMAKE_BINARY_DIR}/manualTests.zip"
+        COMMAND ${CMAKE_COMMAND} -E tar "cf" "${CMAKE_BINARY_DIR}/manualTests.zip" --format=zip --files-from=${CMAKE_BINARY_DIR}/manual_tests.txt
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+endif()
+
+if(NOT TARGET private-test-archive)
+    add_custom_target(private-test-archive
+        COMMAND ${CMAKE_COMMAND} -E remove -f "${CMAKE_BINARY_DIR}/privateTests.zip"
+        COMMAND ${CMAKE_COMMAND} -E tar "cf" "${CMAKE_BINARY_DIR}/privateTests.zip" --format=zip --files-from=${CMAKE_BINARY_DIR}/private_tests.txt
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+endif()
+
+if(NOT TARGET test-archive)
+    add_custom_target(test-archive
+        COMMAND ${CMAKE_COMMAND} -E remove -f "${CMAKE_BINARY_DIR}/tests.zip"
+        COMMAND ${CMAKE_COMMAND} -E tar "cf" "${CMAKE_BINARY_DIR}/tests.zip" --format=zip --files-from=${CMAKE_BINARY_DIR}/tests.txt
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+endif()
+
 macro(add_qt_test TEST_NAME SRCS)
     find_package(Qt5Test REQUIRED)
 
@@ -6,6 +32,8 @@ macro(add_qt_test TEST_NAME SRCS)
     add_test(NAME ${TEST_NAME} COMMAND $<TARGET_FILE:${TEST_NAME}>)
 
     target_link_libraries(${TEST_NAME} PUBLIC Qt5::Test)
+    
+    file(APPEND ${CMAKE_BINARY_DIR}/tests.txt ${TEST_NAME}${CMAKE_EXECUTABLE_SUFFIX}\n)
 endmacro()
 
 option(PRIVATE_TESTS_ENABLED "Enable private tests" ON)
@@ -13,6 +41,7 @@ macro(add_private_qt_test TEST_NAME SRCS)
     if(DEFINED PRIVATE_TESTS_ENABLED)
         if(${PRIVATE_TESTS_ENABLED})
             add_qt_test(${TEST_NAME} "${SRCS}")
+            file(APPEND ${CMAKE_BINARY_DIR}/private_tests.txt ${TEST_NAME}${CMAKE_EXECUTABLE_SUFFIX}\n)
         endif(${PRIVATE_TESTS_ENABLED})
     endif(DEFINED PRIVATE_TESTS_ENABLED)
 endmacro()
@@ -22,7 +51,8 @@ macro(add_manual_qt_test TEST_NAME SRCS)
     find_package(Qt5Test REQUIRED)
     add_executable(${TEST_NAME} ${SRCS})
     target_link_libraries(${TEST_NAME} PUBLIC Qt5::Test)
-
+    file(APPEND ${CMAKE_BINARY_DIR}/manual_tests.txt ${TEST_NAME}${CMAKE_EXECUTABLE_SUFFIX}\n)
+    
     if(DEFINED MANUAL_TESTS_ENABLED)
         if(${MANUAL_TESTS_ENABLED})
             add_test(NAME ${TEST_NAME} COMMAND $<TARGET_FILE:${TEST_NAME}>)
