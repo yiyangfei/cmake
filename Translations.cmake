076fc6faefa4a528f3cda6705d6948d4c122e60d
Frederik Vannoote <frederik.vannoote@barco.com>
2019-02-03 10:37:45 +0100

    Cleanup translations.

--- Translations.cmake
+++ Translations.cmake
@@ -7,15 +7,15 @@ if(Qt5LinguistTools_FOUND)
     
     message(STATUS "Qt5Linguist tools found: 'translate' build target will be created. Use this build target to generate the .TS and .QM files.")
     
+    file(MAKE_DIRECTORY ${TRANSLATIONS_OUTPUT_DIR})
+
     set(TRANSLATION_OUTPUT_FILES "")
     foreach(LOCALE ${TRANSLATION_LOCALES})
         list(APPEND TRANSLATION_OUTPUT_FILES "${TRANSLATIONS_OUTPUT_DIR}/${LOCALE}.ts")
     endforeach()
-    message(STATUS "TRANSLATION_OUTPUT_FILES: ${TRANSLATION_OUTPUT_FILES}")
-    
+
     # Generate TS and QM files
     qt5_create_translation(QM_FILES ${CMAKE_SOURCE_DIR} ${TRANSLATION_OUTPUT_FILES})
-    set(QM_FILES ${QM_FILES} PARENT_SCOPE)
 
     # Create build target
     add_custom_target(translate ALL DEPENDS ${QM_FILES})
