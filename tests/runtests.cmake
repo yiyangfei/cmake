02240d2254d627dae2d93c17426ff7887b4b63a4
Frederik Vannoote <frederik.vannoote@barco.com>
2019-02-08 21:10:43 +0100

    More logging in tests.

--- tests/runtests.cmake
+++ tests/runtests.cmake
@@ -9,6 +9,6 @@ message(STATUS ${OUTPUT})
 
 if(${CMAKE_BUILD_TYPE} STREQUAL "Debug")
     if(NOT ${RESULT} EQUAL 1)
-        message(FATAL_ERROR "Valgrind should find an memory leak")
+        message(FATAL_ERROR "Valgrind should find an memory leak. Return code was: ${RESULT}")
     endif()
 endif()
